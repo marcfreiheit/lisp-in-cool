014bf008bfaa0a9608974072b20a687c2819cd66
Marc André Freiheit <marcandre@freiheit.software>
2017-11-16 12:40:07 +0100

    Setup new project stucture and add test project

---
+++ lisp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lisp_in_cool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> ");
+            var input = Console.ReadLine();
+            while (true) {
+                parse(input);
+                Console.WriteLine(">>> ");
+                input = Console.ReadLine();
+            }
+
+        }
+        private static List<string> parse(string input) {
+           return new List<string>(); 
+        }
+    }
+}
