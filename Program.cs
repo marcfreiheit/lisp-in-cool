974f3752b4f57a738dfc557044b10aafd3211361
Marc André Freiheit <marcandre@freiheit.software>
2017-11-16 11:28:09 +0100

    Add initial project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lisp_in_cool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
